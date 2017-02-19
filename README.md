d6c557a4008cab508443acf5effc8b83381eb03a
Jesse Kleve <jesse.kleve@gmail.com>
2017-02-19 12:30:51 -0700

    cleaned up main

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # README #
 
-This README would normally document whatever steps are necessary to get your application up and running.
+### Useful Sites ###
+
+* [APM 2.6 Board](http://ardupilot.org/copter/docs/common-apm25-and-26-overview.html)
+* [Atmega 2560 Datasheet](http://www.atmel.com/devices/atmega2560.aspx)
+
+* [AVR Libc Reference Manual](http://www.atmel.com/webdoc/avrlibcreferencemanual/index.html)
+* [AVR Fuse Calculator](http://www.engbedded.com/fusecalc/)
+* [Timer Calculator](http://eleccelerator.com/avr-timer-calculator/)
 
 ### What is this repository for? ###
 
@@ -28,11 +35,3 @@ This README would normally document whatever steps are necessary to get your app
 * Repo owner or admin
 * Other community or team contact
 
-### Useful Sites ###
-
-* [APM 2.6 Board](http://ardupilot.org/copter/docs/common-apm25-and-26-overview.html)
-* [Atmega 2560 Datasheet](http://www.atmel.com/devices/atmega2560.aspx)
-
-* [AVR Libc Reference Manual](http://www.atmel.com/webdoc/avrlibcreferencemanual/index.html)
-* [AVR Fuse Calculator](http://www.engbedded.com/fusecalc/)
-* [Timer Calculator](http://eleccelerator.com/avr-timer-calculator/)
--- flight_code/quad/src/main.cpp
+++ flight_code/quad/src/main.cpp
@@ -66,146 +66,70 @@ extern "C" {
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-//#include "clock_speed.h"
-#include <util/delay.h> /* TODO temporary for debugging */
+#include <util/delay.h> // TODO temporary for debugging
 #include <stdint.h>
-
-//#include "led.h"
-//#include "comm.h"
 }
 
+// TODO find a better way to define this for whole project
 #ifndef F_CPU
 #define F_CPU 8000000UL
 #endif
 
 #include "QuadMgr.h"
-//#include "CommsMgr.h"
-//#include "QuadState.h"
-//#include "Eeprom.h"
-//#include "InterruptMgr.h"
 
 //#define RX_BUFFER_SIZE 128
 
-struct data {
-  int8_t roll;
-  int8_t pitch;
-  int8_t yaw;
-} data_t;
-
-/*char rxBuffer[RX_BUFFER_SIZE];
-uint8_t rxReadPos = 0;
-uint8_t rxWritePos = 0;
-
-char getChar(void);*/
-
-volatile uint8_t rx = 0;
-volatile uint8_t tx = 0;
-volatile uint8_t can_tx = 0;
-volatile uint8_t received = 0;
-//volatile char transmit = 0;
+//struct data {
+//  int8_t roll;
+//  int8_t pitch;
+//  int8_t yaw;
+//} data_t;
+//
+//char rxBuffer[RX_BUFFER_SIZE];
+//uint8_t rxReadPos = 0;
+//uint8_t rxWritePos = 0;
+//
+//char getChar(void);
 
 int main( void ) // TODO write a test that read 4 - 5 bytes with a blocking statment
 // before you start reading, start a timer, read the bytes, stop the timer, send the time back or 
 // write to EEPROM. i want to see if thats feasible to read them all in a row
 {
-    //Eeprom::EepromMgr eeprom();
-
     // Initialize
     sei();
     Quad::QuadMgr quadMgr;
-    //Quad::QuadState &quadState = Quad::QuadState::reference();
-    //Quad::InterruptMgr &intMgr = Quad::InterruptMgr::reference();
-    //Comms::CommsMgr comms;
 
     // Process loop
     quadMgr.start();
 }
-//    while( !quadState.abort() )
-//    {
-//        intMgr.pollInterupts();
-//
-//        if( quadState.received() )
-//        {
-//            //uint8_t data = comms.getChar();
-//        }
-//
-//        if( quadState.canTx() )
-//        {
 
-        //}
-//    }
-
-//  INIT_LEDS;
-//
-//  usart_comm_init();
-//
-//  sei();									  /* enable global interrupts */
-//
-//  LED_ON(ORANGE);
-//  _delay_ms(1000);
-//  LED_OFF(ORANGE);
-//
-//  while (1)
-//  {
-//    if ( (UCSR0A & (1<<UDRE0)) ) can_tx = 1; // tx buffer empty
-//
-//    if ( (UCSR0A & (1<<RXC0)) ) rx = 1; // rx buffer has data
-//
-//    if (rx) // read data received
-//    {
-//      received = (uint8_t) UDR0;
-//      rx = 0;
-//
-//      //transmit = received;
-//      tx = 1;
-//    }
-//
-//    if (tx && can_tx) // send data
-//    {
-//      UDR0 = received;
-//      //UDR0 = 42;
-//      can_tx = 0;
-//      tx = 0;
-//    }
-//
-//
-//  }
-//}
-
-//ISR( USART0_RX_vect )
+//char peekChar(void)
 //{
-//  LED_ON(BLUE);
-//  _delay_ms(5000);
-//  LED_OFF(BLUE);
+//  char ret = '\0';
+//
+//  if (rxReadPos != rxWritePos)
+//  {
+//    ret = rxBuffer[rxReadPos];
+//  }
+//
+//  return ret;
 //}
-/*
-char peekChar(void)
-{
-  char ret = '\0';
-
-  if (rxReadPos != rxWritePos)
-  {
-    ret = rxBuffer[rxReadPos];
-  }
-
-  return ret;
-}
-
-char getChar(void)
-{
-  char ret = '\0';
-
-  if (rxReadPos != rxWritePos)
-  {
-    ret = rxBuffer[rxReadPos];
-
-    rxReadPos++;
-
-    if (rxReadPos >= RX_BUFFER_SIZE)
-    {
-      rxReadPos = 0;
-    }
-  }
-
-  return ret;
-}*/
+//
+//char getChar(void)
+//{
+//  char ret = '\0';
+//
+//  if (rxReadPos != rxWritePos)
+//  {
+//    ret = rxBuffer[rxReadPos];
+//
+//    rxReadPos++;
+//
+//    if (rxReadPos >= RX_BUFFER_SIZE)
+//    {
+//      rxReadPos = 0;
+//    }
+//  }
+//
+//  return ret;
+//}
